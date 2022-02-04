20d5c47e288c97a1c71c08aca98dba909323c380
seyma <seyma.firat@skinvision.com>
2022-02-04 22:58:03 +0100

    start

---
+++ mythgames/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Home from './components/home/home'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div>
+      <Home/>
+      </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ mythgames/src/components/home/home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './home.css';
+import '../../imgs/logo.png'
+
+function Home(props) {
+    return (
+        <div>
+        <div className='placeholder'>
+        <header>COMING SOON</header>
+        <img></img>
+        </div>
+        </div>
+        
+    );
+}
+
+export default Home;
