d90b8ed00e993a60e650d39ab67c6594d62040d0
seymafirat <seyma.firat@skinvision.com>
2022-05-24 23:57:06 +0200

    index.js changed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import NavBar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
+import About from './components/about/About'
+import Home from './components/home/Home'
+import Games from './components/games/Games'
+import Contact from './components/contact/Contact'
+
+const Routing = () => {
+  return(
+    <Router>
+      <NavBar/>
+      <Routes>
+      <Route path="/" exact component={Home} />
+        <Route path="/games" exact component={Games} />
+        <Route path="/about" exact component={About} />
+        <Route path="/contact" exact component={Contact} />
+      </Routes>
+      <Footer/>
+    </Router>
+  )
+}
+
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
-
-  document.getElementById("root")
+  <React.StrictMode>
+    <Routing />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
