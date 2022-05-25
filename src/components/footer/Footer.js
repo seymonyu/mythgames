c273112e151e58b4ebe8432a208eb26b704d4f1f
seymafirat <seyma.firat@skinvision.com>
2022-05-25 17:34:49 +0200

    link to youtube video

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer(props) {
     return (
         <div>
-           contact us via <span><a href="mailto:someone@yoursite.com">  contact@mythgamestudio.com</a></span>
+           contact us via <span><a href="mailto:contact@mythgamestudio.com"> contact@mythgamestudio.com</a></span>
         </div>
     );
 }
